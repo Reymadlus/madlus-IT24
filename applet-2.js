6f9d7b7218e902f60a3022f24c129ce3ee3c05d9
Reymadlus <Reymadlus741@gmail.com>
2024-10-07 14:15:53 +0800

    added initilelayer

--- applet-2.js
+++ applet-2.js
@@ -3,4 +3,28 @@ class leafLetMap{
         this.map = L.map(containerId).setview(center, zoom);
         this.initTileLayer();
     }
+
+initTileLayer() {
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
 }
+addMaker(lat, long, message){
+    const marker = L.marker([lat, long]).addTo(this.map)
+    .bindPopup(message);
+}
+    loadMarkersFromJson(url) {
+        fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(marker => {
+                this.addMaker(marker.latitude, marker.longitude, marker.meassage);
+            });
+       })
+       .catch(error => console.error)("Error loading servers:", error);
+    }
+
+}
+
+
+
