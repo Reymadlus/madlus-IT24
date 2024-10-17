130da867d312a9e73a25775d710c6ddf9514f4a0
Reymadlus <Reymadlus741@gmail.com>
2024-10-17 13:06:57 +0800

    added constructor and text input

---
+++ applet-5.js
@@ -0,0 +1,6 @@
+class WeatherApp {
+    constructor(apiKey) {
+        this.apiKey = apiKey;
+       
+        this.cityInput = document.getElementById('cityInput');
+        this.getWeatherBtn = document.getElementById('getWeatherBtn');
